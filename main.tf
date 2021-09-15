d65371b52df0e00f6fff298ad9a59002dc0126c1
Oleksiy Maksymenko <omaksymenko@agilevision.io>
2021-09-15 12:25:20 +0300

    adding cicd

--- main.tf
+++ main.tf
@@ -1,50 +1,142 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+terraform { 
+
+  required_providers { 
+  aws = { 
+    source  = "hashicorp/aws" 
+    version = "~> 3.0" 
+    } 
+  } 
+
+  backend "s3" { 
+    bucket = "state-terraform-my-task" 
+    key    = "terraform.tfstate" 
+    region = "eu-west-3" 
+  }
+} 
+
+provider "aws" { region= "eu-west-3" } 
+
+resource "aws_security_group" "sg" { vpc_id = aws_vpc.vpc_b.id } 
+
+resource "aws_security_group_rule" "ssh" { 
+  from_port = 0 
+  protocol = "-1" 
+  security_group_id = aws_security_group.sg.id 
+  to_port           = 0 
+  type              = "ingress" 
+  cidr_blocks       = ["0.0.0.0/0"] 
+} 
+
+resource "aws_security_group_rule" "out_all" { 
+  from_port         = 0 
+  protocol          = "-1" 
+  security_group_id = aws_security_group.sg.id 
+  to_port           = 0 
+  type              = "egress" 
+  cidr_blocks       = ["0.0.0.0/0"] 
+} 
+
+resource "aws_ecr_repository" "postrge_sql" { 
+  name                 = "container_postgresql" 
+  image_tag_mutability = "MUTABLE" 
+} 
+  
+resource "aws_ecr_repository" "node_js" { 
+  name                 = "container_node" 
+  image_tag_mutability = "MUTABLE" 
+} 
+
+ 
+resource "aws_iam_role" "tf-codepipeline-role" {
+  name = "tf-codepipeline-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
     }
-  }
+  ]
+}
+EOF
 
-  backend "s3" {
-    bucket = "state-terraform-my-task"
-    key    = "state"
-    region = "eu-west-3"
-  }
 }
 
-provider "aws" {
-    region= "eu-west-3"
-}
+data "aws_iam_policy_document" "tf-cicd-pipeline-policies" { 
+  statement{ 
+    sid = "" 
+    actions = ["codestar-connections:UseConnection"] 
+    resources = ["*"] 
+    effect = "Allow" 
+  } 
+  statement{ 
+    sid = "" 
+    actions = ["cloudwatch:*", "s3:*", "codebuild:*"] 
+    resources = ["*"] 
+    effect = "Allow" 
+  } 
+} 
 
-resource "aws_security_group" "sg" {
-  vpc_id = aws_vpc.vpc_b.id
-}
+resource "aws_iam_policy" "tf-cicd-pipeline-policy" { 
+  name = "tf-cicd-pipeline-policy" 
+  path = "/" 
+  description = "Pipeline policy" 
+  policy = data.aws_iam_policy_document.tf-cicd-pipeline-policies.json 
+} 
 
-resource "aws_security_group_rule" "ssh" {
-  from_port         = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.sg.id
-  to_port           = 0
-  type              = "ingress"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
+resource "aws_iam_role_policy_attachment" "tf-cicd-pipeline-attachment" { 
+  policy_arn = aws_iam_policy.tf-cicd-pipeline-policy.arn 
+  role = aws_iam_role.tf-codepipeline-role.id 
+} 
 
-resource "aws_security_group_rule" "out_all" {
-  from_port         = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.sg.id
-  to_port           = 0
-  type              = "egress"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
+resource "aws_iam_role" "tf-codebuild-role" {
+  name = "tf-codebuild-role"
 
-resource "aws_ecr_repository" "postrge_sql" {
-  name                 = "container_postgresql"
-  image_tag_mutability = "MUTABLE"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
 }
+EOF
 
-resource "aws_ecr_repository" "node_js" {
-  name                 = "container_node"
-  image_tag_mutability = "MUTABLE"
+}
+data "aws_iam_policy_document" "tf-cicd-build-policies" { 
+  statement{ 
+    sid = "" 
+    actions = ["logs:*", "s3:*", "codebuild:*", "secretsmanager:*","iam:*"] 
+    resources = ["*"] 
+    effect = "Allow" 
+  } 
+} 
+
+resource "aws_iam_policy" "tf-cicd-build-policy" { 
+  name = "tf-cicd-build-policy" 
+  path = "/" 
+  description = "Codebuild policy" 
+  policy = data.aws_iam_policy_document.tf-cicd-build-policies.json 
+} 
+
+resource "aws_iam_role_policy_attachment" "tf-cicd-codebuild-attachment1" { 
+  policy_arn  = aws_iam_policy.tf-cicd-build-policy.arn 
+  role = aws_iam_role.tf-codebuild-role.id 
+} 
+
+resource "aws_iam_role_policy_attachment" "tf-cicd-codebuild-attachment2" { 
+  policy_arn  = "arn:aws:iam::aws:policy/PowerUserAccess" 
+  role        = aws_iam_role.tf-codebuild-role.id 
 }
